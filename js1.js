80b3ab87af8b9a113a2a1cd97d12c02793e4420d
Polina Namakanova <namakanova.ars@gmail.com>
2025-01-21 12:45:58 +0300

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,16 @@
+// задача 1
+const str = "abcde"
+console.log(str[0], str[1], str[4]);
+
+// задача 2
+let a = parseInt(prompt ("Введите первое число:"));
+let b = parseInt(prompt ("Введите второе число:"));
+console.log(a > 0 || b > 0);
+
+// задача 3
+let age = parseInt(prompt("Введите ваш возраст:"));
+if (age >= 18) {
+    console.log("Совершеннолетний");
+} else {
+    console.log("Несовершеннолетний");
+}
